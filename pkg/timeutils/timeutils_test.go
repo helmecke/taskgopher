879693a317b16efbb1756c8f36b8cae67abdabea
helmecke <j.helmecke@gmail.com>
2022-05-20 20:18:11 +0200

    refactor(timeutils): migrate to table bases tests

--- pkg/timeutils/timeutils_test.go
+++ pkg/timeutils/timeutils_test.go
@@ -9,220 +9,43 @@ import (
 
 func TestDiff(t *testing.T) {
 	t.Parallel()
-
-	t.Run("seconds", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Second), false)
-		want := "1s"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("seconds past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Second), dt, true)
-		want := "-1s"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("seconds future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Second), true)
-		want := "+1s"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("minutes", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Minute), false)
-		want := "1m"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("minutes past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Minute), dt, true)
-		want := "-1m"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("minutes future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Minute), true)
-		want := "+1m"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("hours", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour), false)
-		want := "1h"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("hours past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Hour), dt, true)
-		want := "-1h"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("hours future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour), true)
-		want := "+1h"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("days", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24), false)
-		want := "1d"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("days past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Hour*24), dt, true)
-		want := "-1d"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("days future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24), true)
-		want := "+1d"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("months", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24*7*5), false)
-		want := "1M"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("months past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Hour*24*7*5), dt, true)
-		want := "-1M"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("months future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24*7*5), true)
-		want := "+1M"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("years", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24*7*53), false)
-		want := "1y"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("years past", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt.Add(time.Hour*24*7*53), dt, true)
-		want := "-1y"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
-
-	t.Run("years future", func(t *testing.T) {
-		t.Parallel()
-		dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
-
-		got := timeutils.Diff(dt, dt.Add(time.Hour*24*7*53), true)
-		want := "+1y"
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	})
+	dt := time.Date(2020, 10, 32, 0, 0, 0, 0, time.UTC)
+	type args struct {
+		a    time.Time
+		b    time.Time
+		sign bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"seconds", args{a: dt, b: dt.Add(time.Second), sign: false}, "1s"},
+		{"seconds future", args{a: dt, b: dt.Add(time.Second), sign: true}, "+1s"},
+		{"seconds past", args{a: dt.Add(time.Second), b: dt, sign: true}, "-1s"},
+		{"minutes", args{a: dt, b: dt.Add(time.Minute), sign: false}, "1m"},
+		{"minutes future", args{a: dt, b: dt.Add(time.Minute), sign: true}, "+1m"},
+		{"minutes past", args{a: dt.Add(time.Minute), b: dt, sign: true}, "-1m"},
+		{"hours", args{a: dt, b: dt.Add(time.Hour), sign: false}, "1h"},
+		{"hours future", args{a: dt, b: dt.Add(time.Hour), sign: true}, "+1h"},
+		{"hours past", args{a: dt.Add(time.Hour), b: dt, sign: true}, "-1h"},
+		{"days", args{a: dt, b: dt.Add(time.Hour * 24), sign: false}, "1d"},
+		{"days future", args{a: dt, b: dt.Add(time.Hour * 24), sign: true}, "+1d"},
+		{"days past", args{a: dt.Add(time.Hour * 24), b: dt, sign: true}, "-1d"},
+		{"months", args{a: dt, b: dt.Add(time.Hour * 24 * 7 * 5), sign: false}, "1M"},
+		{"months future", args{a: dt, b: dt.Add(time.Hour * 24 * 7 * 5), sign: true}, "+1M"},
+		{"months past", args{a: dt.Add(time.Hour * 24 * 7 * 5), b: dt, sign: true}, "-1M"},
+		{"years", args{a: dt, b: dt.Add(time.Hour * 24 * 7 * 53), sign: false}, "1y"},
+		{"years future", args{a: dt, b: dt.Add(time.Hour * 24 * 7 * 53), sign: true}, "+1y"},
+		{"years past", args{a: dt.Add(time.Hour * 24 * 7 * 53), b: dt, sign: true}, "-1y"},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := timeutils.Diff(tt.args.a, tt.args.b, tt.args.sign); got != tt.want {
+				t.Errorf("Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
