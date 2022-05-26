69639d5bb5e964c96f0bfc48e22065cb4d11ceea
helmecke <j.helmecke@gmail.com>
2022-05-26 22:06:50 +0200

    feat(sliceutils): int slices

--- pkg/sliceutils/sliceutils.go
+++ pkg/sliceutils/sliceutils.go
@@ -16,3 +16,20 @@ func StrIndexOf(slice []string, value string) int {
 func StrSliceContains(slice []string, value string) bool {
 	return StrIndexOf(slice, value) != -1
 }
+
+// IntIndexOf get the index of the given value in the given string slice,
+// or -1 if not found.
+func IntIndexOf(slice []int, value int) int {
+	for i, v := range slice {
+		if v == value {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// IntSliceContains returns true if string is in slice of strings
+func IntSliceContains(slice []int, value int) bool {
+	return IntIndexOf(slice, value) != -1
+}
