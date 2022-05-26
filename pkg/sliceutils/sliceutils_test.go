0eceea3497494533d2926424c684349cd00b0911
helmecke <j.helmecke@gmail.com>
2022-05-26 22:06:50 +0200

    feat(sliceutils): int slices

--- pkg/sliceutils/sliceutils_test.go
+++ pkg/sliceutils/sliceutils_test.go
@@ -56,3 +56,54 @@ func TestStrSliceContains(t *testing.T) {
 		})
 	}
 }
+
+func TestIntIndexOf(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		slice []int
+		value int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"not found", args{slice: []int{1, 2, 4, 5}, value: 6}, -1},
+		{"found at 0", args{slice: []int{1, 2, 4, 5}, value: 1}, 0},
+		{"found at 1", args{slice: []int{1, 2, 4, 5}, value: 2}, 1},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := sliceutils.IntIndexOf(tt.args.slice, tt.args.value); got != tt.want {
+				t.Errorf("IntIndexOf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIntSliceContains(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		slice []int
+		value int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"not found", args{slice: []int{1, 2, 4, 5}, value: 6}, false},
+		{"found", args{slice: []int{1, 2, 4, 5}, value: 1}, true},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := sliceutils.IntSliceContains(tt.args.slice, tt.args.value); got != tt.want {
+				t.Errorf("IntSliceContains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
