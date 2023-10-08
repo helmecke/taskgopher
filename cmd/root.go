2e1ffa908e846f756b84de67e0841b7d76609eaa
helmecke <j.helmecke@gmail.com>
2023-10-08 18:54:03 +0200

    revert(log): add debug
    This reverts commit 7375c650fe91fb4ae6f7f04079dc2603b75d36f1.

--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,20 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/helmecke/taskgopher/internal/config"
 	"github.com/helmecke/taskgopher/internal/parser"
 )
 
-var (
-	cfgFile string
-	debug   bool
-)
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:    "taskgopher",
-	PreRun: toggleDebug,
-	RunE:   listRunE,
+	Use:  "taskgopher",
+	RunE: listRunE,
 }
 
 // TODO: replace if cobra 1.5 hits - https://github.com/spf13/cobra/pull/1551
@@ -28,12 +22,6 @@ var (
 	filter *parser.Filter
 	mod    *parser.Modification
 )
-
-type PlainFormatter struct{}
-
-func (f *PlainFormatter) Format(entry *log.Entry) ([]byte, error) {
-	return []byte(fmt.Sprintf("%s\n", entry.Message)), nil
-}
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -57,21 +45,10 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.taskgopher.yaml)")
-	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "verbose logging")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	config.Init(cfgFile)
-}
-
-func toggleDebug(cmd *cobra.Command, args []string) {
-	if debug {
-		log.Info("Debug logs enabled")
-		log.SetLevel(log.DebugLevel)
-		log.SetFormatter(&log.TextFormatter{})
-	} else {
-		plainFormatter := new(PlainFormatter)
-		log.SetFormatter(plainFormatter)
-	}
 }
